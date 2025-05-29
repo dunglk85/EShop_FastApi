91bb4461d837798dc37c0038da61c9fe52a82ac1
dunglk85 <dunglk85@gmail.com>
2025-05-29 19:27:39 +0700

    update

--- Product/src/models/product.py
+++ Product/src/models/product.py
@@ -11,3 +11,8 @@ class Product(ProductBase, table=True, inherit=True, mixin=AuditMixin):
 
 class ProductPublic(ProductBase):
     id: int
+
+class ProductUpdate(SQLModel):
+    name: str | None = Field(None, min_length=3, max_length=100, index=True)
+    description: str | None = Field(None, max_length=500, index=True)
+    price: float | None = Field(None, gt=0)
